d97335a1b3e1e0d59b9e9a207df5209d78ee3a26
shreeee4 <103019807+shreeee4@users.noreply.github.com>
2023-10-15 12:01:44 +1100

    Update insert_sql.py
    changed the commands to make the file_name variable

--- Assignment2-group2-19/backend/insert_sql.py
+++ Assignment2-group2-19/backend/insert_sql.py
@@ -3,71 +3,73 @@ from datetime import date
 from process_slither import process_vulnerabilities
 from database import *
 
-# Open the file and read the vulnerabilities
-vulnerabilities = process_vulnerabilities()
 
-db_config = database_config()
-structure = database_structure()
-vulnerabilitiy_statements = valnrabilaties_insert()
+def initialize_database():
+    db_config = database_config()
+    structure = database_structure()
+    vulnerabilitiy_statements = valnrabilaties_insert()
 
-try:
-    # Establish a connection to the MySQL server
-    conn = mysql.connector.connect(**db_config)
-    print("Connection established")
-    # Create a cursor object
-    cursor = conn.cursor()
-    print("Cursor created")
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
 
-    # Check if all the tables exist, create them if not
-    for create_table_statements in structure:
-        cursor.execute(create_table_statements)
+        for create_table_statements in structure:
+            cursor.execute(create_table_statements)
 
-    print("Tables Created")
+        cursor.execute("SELECT COUNT(*) FROM Vulnerabilities")
+        Vulnerabilitiy_amount = cursor.fetchone()[0]
+        if Vulnerabilitiy_amount == 0:
+            for create_vulnerability_statement in vulnerabilitiy_statements:
+                cursor.execute(create_vulnerability_statement)
+                # commits the changes
+        conn.commit()
+        cursor.close()
+        conn.close()
 
-    # Check if the Vulnerabilities table is empty
-    cursor.execute("SELECT COUNT(*) FROM Vulnerabilities")
-    print("Count executed")
-    Vulnerabilitiy_amount = cursor.fetchone()[0]
-    if Vulnerabilitiy_amount == 0:
-        for create_vulnerability_statement in vulnerabilitiy_statements:
-            cursor.execute(create_vulnerability_statement)
-        print("Vulnerability table populated")
+    except mysql.connector.Error as err:
+        print(f"Database Initialization Error: {err}")
+    except Exception as e:
+        print(f"Database Initialization Error: {e}")
 
-    # gets the date the report was made
-    report_date = date.today()
-    report_date = report_date.strftime("%Y-%m-%d")
 
-    # still need to get the contract name from the contract
-    contract_name = 'Sample.sol'
+def insert_vulnerabilities_to_db(vulnerabilities):
+    db_config = database_config()
 
-    # Insert report into the 'reports' table
-    cursor.execute(
-        f"INSERT INTO Reports (contract_name, audit_date) VALUES ('{contract_name}', '{report_date}')"
-    )
-    print("Report table populated")
-    vulnerability_id_list = []
-    for vulnerabilitie_name in vulnerabilities:
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+
+        report_date = date.today().strftime("%Y-%m-%d")
+        contract_name = 'Sample.sol'
+
         cursor.execute(
-            f"SELECT vulnerability_id FROM Vulnerabilities WHERE vulnerability_name = '{vulnerabilitie_name}'")
-        vulnerabilitieID = cursor.fetchone()[0]
-        vulnerability_id_list.append(vulnerabilitieID)
+            f"INSERT INTO Reports (contract_name, audit_date) VALUES ('{contract_name}', '{report_date}')"
+        )
 
-    cursor.execute("SELECT MAX(report_id) FROM reports")
-    report_number = cursor.fetchone()[0]
-    # insert report vulnerability into report vulnerabilities table
-    for vulnerability in vulnerability_id_list:
-        query = "INSERT INTO ReportVulnerabilities (report_id, vulnerability_id) VALUES (%s, %s)"
-        data = (report_number, vulnerability)
-        cursor.execute(query, data)
+        vulnerability_id_list = []
+        for vulnerability_name in vulnerabilities:
+            cursor.execute(
+                f"SELECT vulnerability_id FROM Vulnerabilities WHERE vulnerability_name = '{vulnerability_name}'")
+            vulnerability_id = cursor.fetchone()[0]
+            vulnerability_id_list.append(vulnerability_id)
 
-    # Commit changes and close the cursor and connection
-    conn.commit()
-    cursor.close()
-    conn.close()
+        cursor.execute("SELECT MAX(report_id) FROM reports")
+        report_number = cursor.fetchone()[0]
 
-    print("Vulnerabilities inserted into the database.")
+        for vulnerability in vulnerability_id_list:
+            query = "INSERT INTO ReportVulnerabilities (report_id, vulnerability_id) VALUES (%s, %s)"
+            data = (report_number, vulnerability)
+            cursor.execute(query, data)
 
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
-except Exception as e:
-    print(f"Error: {e}")
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+        return True  # Return True if vulnerabilities are inserted successfully
+
+    except mysql.connector.Error as err:
+        print(f"Vulnerability Insertion Error: {err}")
+        return False  # Return False if an error occurs
+    except Exception as e:
+        print(f"Vulnerability Insertion Error: {e}")
+        return False  # Return False if an error occurs
